b6d211540001841bfc7a63e9213dcefe61e83695
Victor Macedo <victormcdcosta@gmail.com>
2025-08-14 15:10:36 -0300

    Commit de teste

---
+++ teste.py
@@ -0,0 +1,18 @@
+from asyncio import gather, run, sleep
+
+
+async def corrotina(n):
+    print(f'Iniciando corrotina {n}')
+    await sleep(1)
+    print(f'Retomando corrotina {n}')
+    await sleep(1)
+    print(f'Retomando corrotina {n}')
+    await sleep(1)
+    print(f'Corrotina {n} finalizada')
+
+
+async def main():
+    await gather(corrotina(1), corrotina(2)) 
+
+
+run(main())
